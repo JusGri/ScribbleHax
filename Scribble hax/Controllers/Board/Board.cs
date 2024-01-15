2444776f93328b374f99413a6ece45555a702dcd
baike <baike@JUSTINOPC>
2024-01-15 10:33:01 +0200

    Add project files.

---
+++ Scribble hax/Controllers/Board/Board.cs
@@ -0,0 +1,19 @@
+﻿namespace Scribble_hax.Controllers.Board
+{
+    /// <summary>
+    /// Class for holding the scribble board matrix and remembering all letters. 
+    /// </summary>
+    public class Board
+    {
+        private char[,] valuesMatrix;
+        private char[,] bonusMatrix;
+
+        public Board()
+        {
+            valuesMatrix = new char[15, 15];
+            bonusMatrix = new char[15, 15];
+
+            BonusTileReader bonusReader = new BonusTileReader();
+        }
+    }
+}
---
+++ Scribble hax/Controllers/Board/BonusTileReader.cs
@@ -0,0 +1,31 @@
+﻿namespace Scribble_hax.Controllers.Board
+{
+    public class BonusTileReader
+    {
+        private string bonusTileBoardFilePath = ".\\Scribble hax\\bin"; 
+        public BonusTileReader() { 
+            
+        }
+
+        public string[,] ReadBonusTiles()
+        {
+            StreamReader reader = new StreamReader(bonusTileBoardFilePath);
+
+            string[,] bonusTiles = new string[15,15]; 
+
+            string line;
+            int lineIndex = 0;
+            while ((line = reader.ReadLine()) != null)
+            {
+                var singleLineBonuses = line.Split(';');
+                var columnIndex = 0;
+                foreach (var tile in singleLineBonuses)
+                {
+
+                    columnIndex++;
+                }
+                lineIndex++;
+            }
+        }
+    }
+}
